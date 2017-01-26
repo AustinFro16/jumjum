69b07d53ccfd98a154efdfe24a878800d201aa94
Cache Makers <Image15@iMac-K.local>
2017-01-26 16:59:35 -0700

    i messed up

--- main.cpp
+++ main.cpp
@@ -11,13 +11,23 @@ void printBoard()
 
     std::cout << board[i][j];
     }
+    std::cout << std::endl;
+
   }
 
 }
 
 int main(){
+
+
   int turn = 0;
   bool gameOver = false;
+  int rowIn;
+  int colIn;
+
+
+
+
 
 std::cout << "WELCOME TO GHOULS AND BALLERINAS" << std::endl;
 
@@ -33,6 +43,18 @@ while(gameOver == false)
       std::cout << "BALLERINA'S TURN" << std::endl;
     }
 
+    std::cout << "PICK YOUR ROW";
+    std::cin >> rowIn;
+    std::cout << "PICK YOUR COLUMN";
+    std::cin >> colIn;
+    if(turn == 0)
+    {
+      board[rowIn][colIn] = "4";
+    }
+    else
+    {
+      board[rowIn][colIn] = "1";
+    }
 
     turn++;
     turn %= 2;
