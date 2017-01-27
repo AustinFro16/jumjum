f8ce9dbaec199936a0fcaa3788556878aff8d836
Cache Makers <Image15@iMac-K.local>
2017-01-26 17:58:29 -0700

    I REALLY messed up.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+
+
 char board[3][3]={{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
 
 void printBoard()
@@ -8,22 +10,41 @@ void printBoard()
   {
     for(int j=0; j<3; j++)
     {
-
     std::cout << board[i][j];
+      if(j < 2){
+    std::cout << "||";
+      }
     }
     std::cout << std::endl;
+    if(i < 2){
+      std::cout << "=======" << std::endl;
 
+    }
+
+  }
+
+}
+
+void makeMove(int turn, int rowIn, int colIn)
+{
+  if(turn == 0)
+  {
+    board[colIn][rowIn] = 'G';
+  }
+  else
+  {
+    board[colIn][rowIn] = 'B';
   }
 
 }
 
 int main(){
 
-
+  Game myGame;
   int turn = 0;
   bool gameOver = false;
-  int rowIn;
   int colIn;
+  int rowIn;
 
 
 
@@ -33,7 +54,7 @@ std::cout << "WELCOME TO GHOULS AND BALLERINAS" << std::endl;
 
 while(gameOver == false)
   {
-    printBoard();
+    myGame.printBoard();
     if(turn == 0)
     {
       std::cout << "GHOUL'S TURN" << std::endl;
@@ -43,21 +64,22 @@ while(gameOver == false)
       std::cout << "BALLERINA'S TURN" << std::endl;
     }
 
-    std::cout << "PICK YOUR ROW";
-    std::cin >> rowIn;
     std::cout << "PICK YOUR COLUMN";
+    std::cin >> rowIn;
+    std::cout << "PICK YOUR ROW";
     std::cin >> colIn;
     if(turn == 0)
     {
-      board[rowIn][colIn] = "4";
+      board[colIn][rowIn] = 'G';
     }
     else
     {
-      board[rowIn][colIn] = "1";
+      board[colIn][rowIn] = 'B';
     }
 
     turn++;
     turn %= 2;
+
 
 
   }
